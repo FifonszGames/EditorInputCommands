7c3ea2b6581c8ec5054c01285c91f376636ff5e4
Fifonsz <fifonsz.games@gmail.com>
2025-03-03 20:25:27 +0100

    [WIP] Initial list identifier tests addition

---
+++ Source/CommandsExtension/Private/Tests/CommandListIdentifier.spec.cpp
@@ -0,0 +1,38 @@
+﻿// Copyright FifonszGames. All Rights Reserved.
+
+
+#include "CommandExtensionTypes.h"
+
+BEGIN_DEFINE_SPEC(FCommandListIdentifierSpec, "CommandsExtension.CommandListIdentifierSpec", EAutomationTestFlags::EngineFilter | EAutomationTestFlags_ApplicationContextMask)
+	FInputBindingManager* InputBindingManager;
+END_DEFINE_SPEC(FCommandListIdentifierSpec)
+
+void FCommandListIdentifierSpec::Define()
+{
+	Describe("FCommandListIdentifier tests ", [this]()
+	{
+		BeforeEach([this]()
+		{
+			InputBindingManager = &FInputBindingManager::Get();
+			check(InputBindingManager != nullptr);
+		});
+
+		It("Identifier should not be valid", [this]()
+		{
+			const FCommandListIdentifier InvalidIdentifier = FCommandListIdentifier(NAME_None);
+			TestFalse("Is identifier not valid", InvalidIdentifier.IsValid());
+		});
+		
+		It("Identifier should be valid", [this]()
+		{
+			//TODO:: this assumes main frame registration, its a bad test!
+			const FCommandListIdentifier ValidIdentifier = FCommandListIdentifier(TEXT("MainFrame"));
+			TestTrue("Is identifier valid", ValidIdentifier.IsValid());
+		});
+		
+		AfterEach([this]()
+		{
+			InputBindingManager = nullptr;
+		});
+	});
+}
