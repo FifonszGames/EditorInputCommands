36a58f54da0d7fcebb7a58dcea29fd7a4edaed4a
Fifonsz <fifonsz.games@gmail.com>
2025-03-11 19:20:18 +0100

    Adjusted some existing testing code to use beforeach and aftereach

--- Source/CommandsExtension/Private/Tests/ExistingContextBinding.spec.cpp
+++ Source/CommandsExtension/Private/Tests/ExistingContextBinding.spec.cpp
@@ -4,10 +4,17 @@
 #include "CommandExtensionTypes.h"
 #include "ScopedCommand.h"
 
-DEFINE_SPEC(FExistingContentBindingSpec, "CommandsExtension.ExistingContentBinding", EAutomationTestFlags::EngineFilter | EAutomationTestFlags_ApplicationContextMask)
+BEGIN_DEFINE_SPEC(FExistingContentBindingSpec, "CommandsExtension.ExistingContentBinding", EAutomationTestFlags::EngineFilter | EAutomationTestFlags_ApplicationContextMask)
+	TSharedPtr<FScopedCommand> ScopedCommand;
+END_DEFINE_SPEC(FExistingContentBindingSpec)
 
 void FExistingContentBindingSpec::Define()
 {
+	BeforeEach([this]()
+	{
+		ScopedCommand = MakeShared<FScopedCommand>();
+	});
+	
 	It("Empty binding should not be valid", [this]()
 	{
 		const FExistingContextBinding InvalidBinding;
@@ -16,18 +23,16 @@ void FExistingContentBindingSpec::Define()
 	
 	It("Should return valid context", [this]()
 	{
-		const FScopedCommand ScopedCommand;
-		const FExistingContextBinding ValidBinding(ScopedCommand.GetBindingContextName());
+		const FExistingContextBinding ValidBinding(ScopedCommand->GetBindingContextName());
 		const TSharedPtr<FBindingContext> Context = ValidBinding.AsContext();
 		TestTrue("Binding is valid", Context.IsValid());
 	});
 
 	It("Should point to the same binding context", [this]()
 	{
-		const FScopedCommand ScopedCommand;
-		const FExistingContextBinding ValidBinding(ScopedCommand.GetBindingContextName());
+		const FExistingContextBinding ValidBinding(ScopedCommand->GetBindingContextName());
 		
-		const TSharedPtr<FBindingContext> ScopedContext = ScopedCommand.GetData().GetContext();
+		const TSharedPtr<FBindingContext> ScopedContext = ScopedCommand->GetData().GetContext();
 		const TSharedPtr<FBindingContext> Binding = ValidBinding.AsContext();
 		
 		TestEqual("Bindings are the same", ScopedContext, Binding);
@@ -35,9 +40,12 @@ void FExistingContentBindingSpec::Define()
 
 	It("Should be valid", [this]()
 	{
-		const FScopedCommand ScopedCommand;
-		const FExistingContextBinding ValidBinding(ScopedCommand.GetBindingContextName());
-		
+		const FExistingContextBinding ValidBinding(ScopedCommand->GetBindingContextName());
 		TestTrue("Binding is valid", ValidBinding.IsValid());
+	});
+	
+	AfterEach([this]()
+	{
+		ScopedCommand.Reset();
 	});
 }
--- Source/CommandsExtension/Private/Tests/InputCommandRegisterData.spec.cpp
+++ Source/CommandsExtension/Private/Tests/InputCommandRegisterData.spec.cpp
@@ -4,10 +4,17 @@
 #include "CommandExtensionTypes.h"
 #include "ScopedCommand.h"
 
-DEFINE_SPEC(FInputCommandRegisterDataSpec, "CommandsExtension.InputCommandRegisterData", EAutomationTestFlags::EngineFilter | EAutomationTestFlags_ApplicationContextMask)
+BEGIN_DEFINE_SPEC(FInputCommandRegisterDataSpec, "CommandsExtension.InputCommandRegisterData", EAutomationTestFlags::EngineFilter | EAutomationTestFlags_ApplicationContextMask)
+	TSharedPtr<FScopedCommand> ScopedCommand;
+END_DEFINE_SPEC(FInputCommandRegisterDataSpec)
 
 void FInputCommandRegisterDataSpec::Define()
 {
+	BeforeEach([this]()
+	{
+		ScopedCommand = MakeShared<FScopedCommand>();
+	});
+	
 	It("Should not be valid", [this]()
 	{
 		const FInputCommandRegisterData InvalidData = FInputCommandRegisterData();
@@ -16,27 +23,29 @@ void FInputCommandRegisterDataSpec::Define()
 	
 	It("Should be valid", [this]()
 	{
-		const FScopedCommand ScopedCommand;
-		const FInputCommandRegisterData& ValidData = ScopedCommand.GetData();
+		const FInputCommandRegisterData& ValidData = ScopedCommand->GetData();
 		TestTrue("Data is not valid", ValidData.IsValid());
 	});
 
 	It("Should return valid context", [this]()
 	{
-		const FScopedCommand ScopedCommand;
-		const FInputCommandRegisterData& ValidData = ScopedCommand.GetData();
+		const FInputCommandRegisterData& ValidData = ScopedCommand->GetData();
 		TSharedPtr<FBindingContext> Context = ValidData.GetContext();
 		TestTrue("Context is valid", Context.IsValid());
 	});
 
 	It("Should return identifier with same context", [this]()
 	{
-		const FScopedCommand ScopedCommand;
-		const FInputCommandRegisterData& ValidData = ScopedCommand.GetData();
+		const FInputCommandRegisterData& ValidData = ScopedCommand->GetData();
 		const FCommandIdentifier Identifier = ValidData.GetIdentifier();
 
 		const TSharedPtr<FBindingContext> IdendifierContext = Identifier.AsContext();
 		const TSharedPtr<FBindingContext> DataContext = ValidData.GetContext();
 		TestEqual("Binding Contexts", IdendifierContext, DataContext);
+	});
+	
+	AfterEach([this]()
+	{
+		ScopedCommand.Reset();
 	});
 }
